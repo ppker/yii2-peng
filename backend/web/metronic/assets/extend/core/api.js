2af956b4bda4c9fcfbf9fb2d8807547385c0720f
ppker <yanzhipeng@jucaicat.com>
2017-02-04 17:33:12 +0800

    优化 ignore

---
+++ backend/web/metronic/assets/extend/core/api.js
@@ -0,0 +1,83 @@
+(function(MAIN, $){
+	
+	"use strict";
+	
+	var self = MAIN.nameSpace.reg("api"),
+		queue = [],
+		ajax = null;
+		
+	ajax = function(options){
+		var ret = null;
+		
+		if(MAIN.define.isAjaxLock){
+			queue.push(options);
+			/*
+			if(typeof options.failCallBack === "function"){
+				options.failCallBack({
+					success: false,
+					message: ZP.msg.ajaxLocked,
+					data: null
+				});
+			}
+			*/
+		}else{
+			
+			options.async = typeof options.async === "undefined" ? true : options.async;
+			options.async = options.successCallBack ? options.async : false;
+			options.dataType = options.dataType ? options.dataType : "json";
+			MAIN.define.isAjaxLock = true;
+
+			$.ajax({
+				async: options.async,
+				// dataType: "json",
+				dataType: options.dataType,
+				type: "post",
+				url: options.url,
+				data: options.data,
+				
+				success: function(result, textStatus){
+					MAIN.define.isAjaxLock = false;
+
+					if(result.success && typeof options.successCallBack === "function"){
+						options.successCallBack(result);
+					}else if(typeof options.failCallBack === "function"){
+						options.failCallBack(result);
+					}
+					ret = result;
+				},
+				error: function(XMLHttpRequest, textStatus, errorThrown){
+					MAIN.define.isAjaxLock = false;
+
+					if(typeof options.failCallBack === "function"){
+						options.failCallBack({
+							success: false,
+							message: errorThrown,
+							data: null
+						});
+					}
+				},
+				complete: function(XMLHttpRequest, textStatus){
+					MAIN.define.isAjaxLock = false;
+
+					if(queue.length >= 1){
+						var options = queue.shift();
+						ajax(options);
+					}
+				}
+			});
+		}
+		return ret;
+	};
+
+	///////////////////////////////////////////////////////////////////////
+
+    // 用户账号信息
+	self.user_index = function(options) {
+        options = options ? options : {};
+        options.url = "/api/web/users/index";
+        return ajax(options);
+	};
+
+
+
+})(ZP, jQuery);
